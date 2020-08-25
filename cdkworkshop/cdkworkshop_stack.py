ffbe2e5086eed66c85fc24452ea1889c8039908d
Manish R Warang <manish6385@gmail.com>
2020-08-25 14:06:16 +0530

    added lambda layer capability

--- cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop_stack.py
@@ -2,6 +2,7 @@ from aws_cdk import (
     core,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
+    aws_iam as iam
 
 )
 
@@ -11,18 +12,36 @@ class CdkworkshopStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        # self.node.apply_aspect(core.Aws.ACCOUNT_ID)
+
+        lambda_iam_role = iam.Role(
+            self, 'lambdaRole',
+            assumed_by=iam.ServicePrincipal('lambda.amazonaws.com'),
+            role_name="lambda-hello-role"
+        )
+
+        lambda_layer = _lambda.LayerVersion(
+            self, 'lambdaLayer',
+            code=_lambda.Code.asset('lambda_layers/python_libs.zip'),
+            compatible_runtimes=[_lambda.Runtime.PYTHON_3_7],
+            layer_version_name='lambda-layer-cdk',
+            description='A Lambda layer for common utility'
+        )
+
         my_lambda = _lambda.Function(
-            self,'HelloHandler',
+            self, 'HelloHandler',
+            role=lambda_iam_role,
             function_name='hellohandler',
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.asset('lambda'),
-            handler='hello.handler'
+            handler='hello.handler',
+            layers=[lambda_layer],
+            environment={
+                'env_test_key': 'AWS CDK',
+            }
         )
 
         apigw.LambdaRestApi(
-            self,'Endpoint',
+            self, 'Endpoint',
             handler=my_lambda,
         )
-
-
-
--- lambda/hello.py
+++ lambda/hello.py
@@ -1,11 +1,17 @@
+import os
 import json
+import custom_func as cf
 
-def handler(event,context):
+
+def handler(event, context):
+    env_test_value = os.environ['env_test_key']
+    print(env_test_value)
+    cf.cust_fun()
     print('request: {}'.format(json.dumps(event)))
     return {
         'statusCode': 200,
         'headers': {
             'Content-Type': 'text/plain'
         },
-        'body': 'Hello, CDK! You have hit {}\n'.format(event['path'])
+        'body': 'Hello, AWS CDK! You have hit {}\n'.format(event['path'])
     }
